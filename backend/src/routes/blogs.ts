c1da278dfec89c7a6b60f66b5bdca03db561b47f
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-03-22 15:59:09 +0530

    Added blog routes and middleware for blosg

--- backend/src/routes/blogs.ts
+++ backend/src/routes/blogs.ts
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
+import { createPostSchema, updatePostSchema } from "../../../zod/src";
 
 export const blogRouter = new Hono<{
   Bindings: {
@@ -10,20 +14,102 @@ export const blogRouter = new Hono<{
   };
 }>();
 
-blogRouter.post("/", (c) => {
-  return c.text("Blog Route");
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const posts = await prisma.post.findMany();
+
+    return c.json(posts);
+  } catch (error) {
+    return c.json({ error });
+  }
 });
 
-blogRouter.put("/", (c) => {
-  return c.text("Signup Route");
+blogRouter.use("/", async (c, next) => {
+  const authHeader = c.req.header("authorization");
+  if (!authHeader) {
+    c.status(411);
+    return c.json({ error: "Empty header" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  const success = await verify(token, c.env.JWT_SECRET);
+
+  if (!success) {
+    c.status(403);
+    return c.json({ error: "Unauthorized" });
+  }
+  c.set("userId", success.id);
+  await next();
+});
+
+blogRouter.post("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const userId = c.get("userId");
+  const body = await c.req.json();
+  const data = createPostSchema.safeParse(body);
+
+  if (!data.success) {
+    return c.json({ error: data.error });
+  }
+
+  const { title, content } = data.data;
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId: userId,
+      },
+    });
+
+    return c.json({ id: post.id });
+  } catch (error) {
+    return c.json({ error });
+  }
+});
+
+blogRouter.put("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const userId = c.get("userId");
+  const body = await c.req.json();
+  const data = updatePostSchema.safeParse(body);
+
+  if (!data.success) {
+    return c.json({ error: data.error });
+  }
+
+  const { title, content, id } = data.data;
+
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id,
+        authorId: userId,
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+
+    return c.json({ message: "Updated post: " + post.id });
+  } catch (error) {
+    return c.json({ error });
+  }
 });
 
 blogRouter.get("/:id", (c) => {
   const id = c.req.param("id");
-  console.log(id);
   return c.text("get blog route");
-});
-
-blogRouter.get("/bulk", (c) => {
-  return c.text("Get blogs in Bulk");
 });
--- zod/src/index.ts
+++ zod/src/index.ts
@@ -11,5 +11,18 @@ export const signinSchema = z.object({
   password: z.string().min(6),
 });
 
+export const createPostSchema = z.object({
+  title: z.string().min(1),
+  content: z.string().min(1),
+});
+
+export const updatePostSchema = z.object({
+  id: z.string(),
+  title: z.string().min(1),
+  content: z.string().min(1),
+});
+
 export type SignupInput = z.infer<typeof signupSchema>;
 export type SigninInput = z.infer<typeof signinSchema>;
+export type CreatePostInput = z.infer<typeof createPostSchema>;
+export type UpdatePostInput = z.infer<typeof updatePostSchema>;
