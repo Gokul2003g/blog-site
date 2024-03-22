d6fd66f8523106e0b5d3d2ddc0171df9a8203f70
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-03-22 12:34:37 +0530

    done with signup and signin routes

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { signupSchema, signinSchema } from "../../../zod/src/index";
+import { sign } from "hono/jwt";
 
 export const userRouter = new Hono<{
   Bindings: {
@@ -15,10 +17,70 @@ userRouter.post("/signup", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+  const data = signupSchema.safeParse(body);
 
-  return c.text("Signup Route");
+  if (!data.success) {
+    return c.json({ error: data.error });
+  }
+
+  const { name, email, password } = data.data;
+
+  try {
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!findUser) {
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      const token = sign({ userId: user.id }, c.env.JWT_SECRET);
+      return c.json({ token });
+    }
+  } catch (error) {
+    return c.json({ error: "Error creating user. Try again later." });
+  }
 });
 
-userRouter.post("/signin", (c) => {
-  return c.text("Signin Route");
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  console.log("HI there");
+
+  const body = await c.req.json();
+  const data = signinSchema.safeParse(body);
+
+  if (!data.success) {
+    return c.json({ error: data.error });
+  }
+
+  const { email, password } = data.data;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+        password,
+      },
+    });
+
+    if (!user) {
+      c.status(404);
+      return c.json({ error: "User not found." });
+    }
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({ token });
+  } catch (error) {
+    return c.json({ error });
+  }
 });
--- zod/src/index.ts
+++ zod/src/index.ts
@@ -10,3 +10,6 @@ export const signinSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export type SignupInput = z.infer<typeof signupSchema>;
+export type SigninInput = z.infer<typeof signinSchema>;
